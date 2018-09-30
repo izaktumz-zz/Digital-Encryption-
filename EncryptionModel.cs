77b19387f94563426bd6e9cbbccb65d4f0cdf4ba
Isaac Tumuti <geoftums@gmail.com>
2018-09-30 09:10:46 +0400

    Add project files.

---
+++ EncryptionModel.cs
@@ -0,0 +1,23 @@
+﻿namespace EncryptionLib
+{
+    /// <summary>
+    /// A Class that holds the encryption details
+    /// </summary>
+    public class EncryptionModel
+    {
+        /// <summary>
+        /// The encrypted message
+        /// </summary>
+        public byte[] EncryptedMessage { get; set; }
+        /// <summary>
+        /// The key used to encrypt the message.Use it to decrypt the message
+        /// </summary>
+        public byte[] Key { get; set; }
+        /// <summary>
+        /// The IV used to encrypt the message.Use it to decrypt the message.
+        /// The IV and the Key are required to encrypt and decrypt the message.
+        /// Store them for later use.
+        /// </summary>
+        public byte[] IV { get; set; }
+    }
+}
---
+++ EncryptionService.cs
@@ -0,0 +1,62 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+namespace EncryptionLib
+{
+    public class EncryptionService
+    {
+        
+        #region Methods
+        /// <summary>
+        /// Returns a model with encryption details required when decrypting data.
+        /// </summary>
+        /// <param name="data">The data to encrypt.</param>
+        /// <returns></returns>
+        public EncryptionModel EncryptData(string data)
+        {
+            using (SymmetricAlgorithm alg=new AesManaged())
+            {
+                ICryptoTransform encryptor = alg.CreateEncryptor(alg.Key, alg.IV);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter sw = new StreamWriter(cs, Encoding.UTF8))
+                        {
+                            sw.Write(data);
+                        }
+                    }
+                    return new EncryptionModel()
+                    {
+                         EncryptedMessage=ms.ToArray(), IV=alg.IV,Key=alg.Key
+                    };
+                }
+            }
+        }
+        /// <summary>
+        /// returns the original message
+        /// </summary>
+        /// <param name="cipherText">the encrypted message</param>
+        /// <param name="key">the key used for encryption</param>
+        /// <param name="iv">the key used for decryption</param>
+        /// <returns></returns>
+        public string DecryptData(byte[]cipherText,byte[] key,byte[]iv)
+        {
+            using (SymmetricAlgorithm alg=new AesManaged())
+            {
+                ICryptoTransform decryptor = alg.CreateDecryptor(key, iv);
+                using (MemoryStream ms = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader sr = new StreamReader(cs))
+                        {
+                            return sr.ReadToEnd();
+                        }
+                    }
+                }
+            }  
+        }
+        #endregion
+    }
+}
